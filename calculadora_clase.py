6b5ec3ad0eb7cbf109409f0c5fc31daf1ed0585b
JorgeM28 <jorge2224764@correo.uis.edu.co>
2022-09-10 08:28:40 -0500

    Se agrego procesing en python

--- calculadora_clase.py
+++ calculadora_clase.py
@@ -22,3 +22,45 @@ print("6. Logaritmo(El logaritmo del primero )")
 
 opcion = int(input("\nDame la opcion: "))
 
+# procesing
+
+if(opcion==1):
+    z= x + y
+    print(x,"+",y)
+elif (opcion==2):
+    z = x - y
+    print(x,"-",y)
+elif(opcion==3):
+    z = x * y
+    print(x,"X",y)
+elif (opcion==4 and y!=0):
+    z = x / y
+    print(x,"/",y)
+elif(opcion==4 and y==0):
+    print("El denominador es igial a cero y")
+    print("NO se puede realizar la division.")
+    bandera= True
+elif (opcion==5):
+    z = pow(x,y)
+    print(x,"^",y)
+elif(opcion==6 and x>0):
+    z=log(x)
+    print("Logaritmo de",x)
+elif(opcion==6 and x<=0):
+    print("El valor de x es <= a cero y")
+    print("NO se puede calcular el logaritmo. ")
+else:
+    print("opcion no valida ")
+
+    # se escribe el resultado con otra condicion 
+if(opcion>7 and bandera==False):
+        print("Resultado =",z)
+
+#fin
+
+
+
+
+
+
+
